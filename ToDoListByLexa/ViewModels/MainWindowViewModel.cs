d70757dbb3faaf3dced8fc8de9a6603a0be876dc
aleksey pavlov <alekseyelit1337@gmail.com>
2021-02-14 17:39:21 +0300

    Привязали свойство заголовка с ViewModel

---
+++ ToDoListByLexa/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ToDoListByLexa.ViewModels.Base;
+
+namespace ToDoListByLexa.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заговок окна
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        private string _title = "Контроль мероприятий";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        #endregion
+    }
+}
