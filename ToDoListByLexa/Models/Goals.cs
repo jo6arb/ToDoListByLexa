7840ef3eb69dda317ff1e80a90427286574f2afc
aleksey pavlov <alekseyelit1337@gmail.com>
2021-02-28 03:53:26 +0300

    Добавили модель ЗАдачи

---
+++ ToDoListByLexa/Models/Goals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ToDoListByLexa.Models
+{
+    internal class Goals
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateAddGoal { get; set; }
+
+        public DateTime DateSuccessGoal { get; set; }
+
+        public DateTime DateConrtolGoal { get; set; }
+
+        public byte Priority { get; set; }
+
+        public string Note { get; set; }
+    }
+}
