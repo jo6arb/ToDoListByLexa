e6a4b78d5fde974fccd0cd6ecb7994ca047718d4
aleksey pavlov <alekseyelit1337@gmail.com>
2021-04-22 20:57:44 +0300

    ShortTaskView теперь  в MVVMCross

--- ToDoListByLexa/Views/UserControls/ShortInfoTaskView.xaml.cs
+++ ToDoListByLexa/Views/UserControls/ShortInfoTaskView.xaml.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MvvmCross.Platforms.Wpf.Views;
 
 namespace ToDoListByLexa.Views.UserControls
 {
     /// <summary>
     /// Логика взаимодействия для ShortInfoTaskView.xaml
     /// </summary>
-    public partial class ShortInfoTaskView : UserControl
+    public partial class ShortInfoTaskView : MvxWpfView
     {
         public ShortInfoTaskView()
         {
